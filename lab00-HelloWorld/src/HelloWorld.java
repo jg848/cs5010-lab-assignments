e3b3d4aa3a9a594af13cfd787bb64223f2c1c630
jg848 <jg848@njit.edu>
2022-02-01 19:10:49 -0500

    lab00-HelloWorld

---
+++ lab00-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * This is an example hello world program.
+ * @author Jaswin Gumpella
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
